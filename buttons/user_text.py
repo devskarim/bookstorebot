70b9f230a2bfd14c9ffa5f3564b37ca6f20c58c5
karimov <devizzatillo@gmail.com>
2025-10-11 16:51:16 +0500

    updating usertexts

--- buttons/user_text.py
+++ buttons/user_text.py
@@ -1,25 +1,10 @@
-REG_TEXT = f"""
-✨ 📚 Kitoblar Olami ✨
-Siz uchun yangi bilim va ilhom manbai
-
-🔍 Izlang | 🛒 Sotib oling
-⭐ Sevimlilarga saqlang | 📜 Tarixni ko'ring
-
-📖 Har bir kitob – yangi dunyo.
-Siz ham o'zingizga mos dunyoni kashf qiling!
-
-⚠️ Ro'yxatdan o'tish shart!
-📲 Pastdagi tugma orqali ro'yxatdan o'ting va barcha imkoniyatlardan to'liq foydalaning.
-
-🔘 [📝 Ro'yxatdan o'tish]
-"""
-
-GET_NAME = """📝 Ismingizni kiriting
+REG_TEXT = f"""📝 Ismingizni kiriting
 
 Sizni yaxshiroq tanishimiz va buyurtmalaringizni qulayroq boshqarishimiz uchun ismingizni kiriting.
 
-✍️ Masalan: Karimov Izzatulloh"""
-
+✍️ Masalan: Karimov Izzatulloh
+📝 Ismingizni kiriting
+"""
 
 GET_PHONE = """
 
