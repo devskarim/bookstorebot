67a06e691bc3465683db9c90a2677a6f79cc7bdb
karimov <devizzatillo@gmail.com>
2025-10-11 16:36:24 +0500

    Fxiing bubgs and ready for database

--- handler/user.py
+++ handler/user.py
@@ -30,7 +30,6 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler('user_edit.log'),
         logging.StreamHandler()
     ]
 )
